45407bf5890f3cd91cf676f60b5ecd00dfc4a93c
jwjb1020 <jwjb1020@gmail.com>
2023-04-06 15:09:08 +0900

    실험중

---
+++ chapter5/Exam01.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+public class Exam01 {
+	private int height;
+	private String name;
+	private int weight;
+	private String color;
+	private boolean isFly;
+
+	public void setName() {
+		String value = "mark6";
+		name = value;
+	}
+
+}
