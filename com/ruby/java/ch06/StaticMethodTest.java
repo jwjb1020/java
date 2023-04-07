819569f750b34893ce217a753093086ccc0d3a77
jwjb1020 <jwjb1020@gmail.com>
2023-04-07 18:11:14 +0900

    class 정의

--- com/ruby/java/ch06/StaticMethodTest.java
+++ com/ruby/java/ch06/StaticMethodTest.java
@@ -8,8 +8,18 @@ public class StaticMethodTest {
 	public void print2() {
 		System.out.println("java");
 	}
+	
 	public static void main(String[] args) {
+		
+		StaticMethodTest s =  new StaticMethodTest();
+		
 		StaticMethodTest.print1();
+		s.print1();
+		print1();
+		
+		StaticMethodTest.print2();
+		s.print2();
+		print2();
 	}
 
 	
