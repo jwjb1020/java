ee9198e0bfed2d5b84ecf3364633389f53661840
jwjb1020 <jwjb1020@gmail.com>
2023-04-11 19:28:49 +0900

    2023.04.11

--- Misson/sumTest.java
+++ Misson/sumTest.java
@@ -3,7 +3,7 @@ package Misson;
 public class sumTest {
 	public static void main(String[] args) {
 		TotalSum ts = new TotalSum();
-		ts.getSum(1000000);
+		ts.getSum(100);
 		System.out.println("전체의 합 :" + ts.getNum());
 		System.out.println("짝수의 합 :" + ts.getEven());
 		System.out.println("홀수의 합 :" + ts.getOdd());
