637cb7ec42ba46b06168f25780acc0c696f853f5
jwjb1020 <jwjb1020@gmail.com>
2023-04-20 00:55:11 +0900

    2023.04.20

---
+++ Backjoon/꼬마정민.java
@@ -0,0 +1,33 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class 꼬마정민 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		System.out.print("");
+		long b = sc.nextLong();
+		System.out.print("");
+		long c = sc.nextLong();
+
+		System.out.println(a + b + c);
+		sc.close();
+	//	\    /\
+	//	 )  ( ')
+	//	(  /  )
+	//	 \(__)|
+		
+//		|\_/|
+//		|q p|   /}
+//		( 0 )"""\
+//		|"^"`    |
+//		||_/=\\__|
+		System.out.println("|\\_/|");
+		System.out.println("|q p|    /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`     |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
